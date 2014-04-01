ee2fe574bea06e7ca79d9de0b83ff50527fc3751
Zachary Spector <zjspector@gmail.com>
2014-04-01 16:13:53 -0400

    Add hadouken sound, animation and playHadouken function
    Thinkful Street Fighter part 4 complete

--- js/app.js
+++ js/app.js
@@ -8,11 +8,18 @@ $(document).ready(function (){
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		// play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// show hadouken and animate it to the right of the screen
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '300px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '-216px');
+			}
+		);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
@@ -20,3 +27,9 @@ $(document).ready(function (){
 		//ryu goes back to his ready position
 	});
 });
+
+function playHadouken() {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
