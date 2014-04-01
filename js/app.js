56094e7629bc985eb7ffc419681f25695f991c1b
Zachary Spector <zjspector@gmail.com>
2014-04-01 15:44:38 -0400

    Add mouseenter, mouseleave, mousedown, .mouseup events to app.js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function (){
+	$('.ryu').mouseenter(function (){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		// play hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// show hadouken and animate it to the right of the screen
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+		//ryu goes back to his ready position
+	});
+});
