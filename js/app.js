08485e9f782b915d81435aac5d05fe45500d0e7f
Zachary Spector <zjspector@gmail.com>
2014-04-03 11:11:43 -0400

    Add keydown and intro function
    Final

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 $(document).ready(function (){
+	var isMouseEnter = false;
+	intro();
+
 	$('.ryu').mouseenter(function (){
+		isMouseEnter = true;
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
 	.mouseleave(function() {
+		isMouseEnter = false;
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
 	})
@@ -26,10 +31,37 @@ $(document).ready(function (){
 		$('.ryu-ready').show();
 		//ryu goes back to his ready position
 	});
+	$(document).keydown(function(event) {
+		if (event.keyCode === 88) {
+			console.log('working');
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(event) {
+		if(event.keyCode === 88){
+			console.log('up');
+			if (isMouseEnter) {
+				$('.ryu-cool').hide();
+				$('.ryu-ready').show();
+			} else {
+				$('.ryu-cool').hide();
+				$('.ryu-still').show();
+			}
+		}
+	});
 });
 
 function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
+}
+
+function intro() {
+	$('.ryu, .instructions').hide();
+	$('.logo').fadeIn('slow');
+	$('.logo').fadeOut('slow');
+	$('.ryu, .instructions').delay(1000).fadeIn('slow');
 }
